79a515a30d39279ff7843d7bc0936f9325c3761e
unknown <delangelj711@gmail.com>
2019-03-23 12:06:19 -0600

    archivo employees.sql actualizado

--- employees.sql
+++ employees.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-03-2019 a las 00:51:20
+-- Tiempo de generación: 23-03-2019 a las 19:04:57
 -- Versión del servidor: 5.6.20
 -- Versión de PHP: 5.5.15
 
@@ -46,7 +46,17 @@ INSERT INTO `employees` (`employee_key`, `name`, `lastname`, `level`, `gender`, 
 ('emp1', 'Brenda', 'Villegas', 1, 'F', '3500.50', 'brenda@correo.com', 'brenda123', '4da938776fdb8a0236a0b980fb27214b'),
 ('emp2', 'Jaime', 'Rocha', 2, 'M', '2800.30', 'jaime@correo.com', 'jaime123', '26eab66329fae7cf234013a4d49eef35'),
 ('emp3', 'Guillermo', 'Ramirez', 3, 'M', '1800.00', 'guillermo@correo.com', 'guillermo123', '16289f7266ad864c3aec42763a75a0f7'),
-('emp4', 'Raquel', 'Zarate', 2, 'F', '2850.00', 'raquel@correo.com', 'raquel123', '7c324d6ebfcd6718de98c51b05620607');
+('emp4', 'Raquel', 'Zarate', 2, 'F', '2850.00', 'raquel@correo.com', 'raquel123', '7c324d6ebfcd6718de98c51b05620607'),
+('emp5', 'Alfredo', 'Torres', 1, 'M', '3500.00', 'alfredo@correo.com', 'alfredo123', 'd59e598dae9859120126d87c91ac6c7e');
+
+--
+-- Disparadores `employees`
+--
+DELIMITER //
+CREATE TRIGGER `createHash` BEFORE INSERT ON `employees`
+ FOR EACH ROW SET NEW.hash = MD5(NEW.password)
+//
+DELIMITER ;
 
 --
 -- Índices para tablas volcadas
